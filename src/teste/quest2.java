b2e96b1a21115fc1e4062da3b39d5e143cd198dd
Guilherme-Dev <guilhermeagripino@gmail.com>
2024-03-07 18:08:04 -0300

    Atualizando quest2

--- src/teste/quest2.java
+++ src/teste/quest2.java
@@ -6,44 +6,39 @@ public class quest2 {
 
 	public static void main(String[] args) {
 
-	    Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
         
-	    System.out.println("Digite um número interio: ");
 	    int n = sc.nextInt();
         int proximo, anterior = 0, atual = 1;
-        int cout = 0;
+        int maximo= n + 2;
+        int cout= 0;
         
-        int fib[] = new int [(n+2)];
-        fib[0]= anterior;
-   		fib[1]= atual;
-   		
-   	
-   		 
-   			for (int i = 2; i <= n; i++) {
-         	
-         	
-         	proximo = anterior + atual;
-         	anterior = atual;
-         	atual = proximo;
-         	
-         	fib[i] = proximo;
-         }
-                		
-         for (int i = 0; i <= n; i++) {
-         	if(n == fib[i]) {
-         		cout = cout + 1;
-         	}
-          	
-         	}
-         
-         if(cout == 1) {
-         	System.out.println("o número " + n +" pertence a sequencia de Fibonacci");
-         }else
-         	System.out.println("o número " + n +" não pertence a sequencia de Fibonacci");
-        
-   		
+        int [] fib = new int [maximo];
+        fib[0] = anterior;
+        fib[1] = atual;
+        	        
+        for (int i = 2; i < maximo; i++) {
 
-   		
+        		System.out.println(anterior + " ");
+        	
+        	proximo = anterior + atual;
+        	anterior = atual;
+        	atual = proximo;
+        	
+        	fib[i]= proximo;
+        	
+        	}
+        for (int i = 0; i <maximo; i++) {
+        	if(n == fib[i]) {
+        		cout = cout + 1;
+        	}
+    	
+    	}
+    
+        if(cout == 1) {
+         	System.out.println("o número " + n +" pertence a sequencia de Fibonacci");
+       }else
+         	System.out.println("o número " + n +" não pertence a sequencia de Fibonacci");
         
          sc.close();
  	}
